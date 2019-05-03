6df062be6dd8cf9da532552e239da33aa2133f6c
biomotion <bensonpuppy1997@gmail.com>
2019-05-03 17:10:55 +0800

    Initial Commit

---
+++ iBeacon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iBeacon
+//
+//  Created by 李聖誠 on 2019/5/3.
+//  Copyright © 2019 nctuesd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
